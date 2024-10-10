6b84ed2f50b1260baac20bcce96d3d43509d9f2c
Nivmizz7 <148060940+Nivmizz7@users.noreply.github.com>
2024-10-10 15:26:55 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,96 @@
 let chf_1 = 0
+let chf_2 = 0
 input.onButtonPressed(Button.A, function () {
-	
-})
-input.onGesture(Gesture.Shake, function () {
     chf_1 = randint(1, 6)
     basic.showString("" + (chf_1))
 })
+input.onButtonPressed(Button.B, function () {
+    chf_2 = randint(2, 12)
+    basic.showNumber(chf_2)
+})
 basic.forever(function () {
-	
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . # # # .
+        . . . # .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . # # . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # # . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . # # # .
+        . # . . .
+        . # # # .
+        `)
 })
