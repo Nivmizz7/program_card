b96794e9e597f38b1a50508461f28479e5ce4c62
Nivmizz7 <148060940+Nivmizz7@users.noreply.github.com>
2024-10-10 15:21:37 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+let chf_1 = 0
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+    chf_1 = randint(1, 6)
+    basic.showString("" + (chf_1))
+})
+basic.forever(function () {
+	
+})
