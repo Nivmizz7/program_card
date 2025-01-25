68ed33705473222476014f157ce7583551cee012
Nivmizz7 <148060940+Nivmizz7@users.noreply.github.com>
2025-01-25 10:53:00 +0100

    changement des boutons

--- main.ts
+++ main.ts
@@ -1,6 +1,64 @@
-let chf_2 = 0
 let chf_1 = 0
+let chf_2 = 0
 input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    chf_1 = randint(1, 6)
+    if (chf_1 == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (chf_1 == 2) {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . # # # .
+            . # . . .
+            . # # # .
+            `)
+    }
+    if (chf_1 == 3) {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . # # # .
+            . . . # .
+            . # # # .
+            `)
+    }
+    if (chf_1 == 4) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # # # .
+            . . . # .
+            . . . # .
+            `)
+    }
+    if (chf_1 == 5) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . . . # .
+            . # # # .
+            `)
+    }
+    if (chf_1 == 6) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     chf_2 = randint(2, 12)
     if (chf_2 == 2) {
@@ -100,64 +158,6 @@ input.onButtonPressed(Button.A, function () {
             . # . # .
             . # # . .
             . # # # #
-            `)
-    }
-})
-input.onGesture(Gesture.Shake, function () {
-    basic.clearScreen()
-    chf_1 = randint(1, 6)
-    if (chf_1 == 1) {
-        basic.showLeds(`
-            . . # . .
-            . # # . .
-            . . # . .
-            . . # . .
-            . . # . .
-            `)
-    }
-    if (chf_1 == 2) {
-        basic.showLeds(`
-            . # # # .
-            . . . # .
-            . # # # .
-            . # . . .
-            . # # # .
-            `)
-    }
-    if (chf_1 == 3) {
-        basic.showLeds(`
-            . # # # .
-            . . . # .
-            . # # # .
-            . . . # .
-            . # # # .
-            `)
-    }
-    if (chf_1 == 4) {
-        basic.showLeds(`
-            . # . # .
-            . # . # .
-            . # # # .
-            . . . # .
-            . . . # .
-            `)
-    }
-    if (chf_1 == 5) {
-        basic.showLeds(`
-            . # # # .
-            . # . . .
-            . # # # .
-            . . . # .
-            . # # # .
-            `)
-    }
-    if (chf_1 == 6) {
-        basic.showLeds(`
-            . # # # .
-            . # . . .
-            . # # # .
-            . # . # .
-            . # # # .
             `)
     }
 })
